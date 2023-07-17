e9919ac27e66018c512ee1f408462f44c287d2db
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-17 14:32:30 +0700

    feat: add logging

--- scripts/meta.js
+++ scripts/meta.js
@@ -6,23 +6,30 @@ const meta = {
 	categories: [],
 };
 
+hexo.log.i(
+	"create meta.json",
+	`can be accessed at ${hexo.config.url}/${hexo.config.root.replace(
+		/\//gm,
+		""
+	)}/meta.json`
+);
+
 hexo.extend.generator.register("meta", function (locals) {
-  locals.tags.sort("name").each(function (tag) {
-    if (!meta.tags.includes(tag.name)) meta.tags.push(tag.name);
-  });
-  locals.categories.sort("name").each(function (category) {
-    if (!meta.categories.includes(category.name))
-      meta.categories.push(category.name);
-  });
-  locals.posts.sort("name").each(function (post) {
-    meta.posts.push({
-      title: post.title,
-      url: encodeURI(post.permalink),
-      // get modified date first
-      date:
-        post.updated.toDate().toISOString() ||
-        post.date.toDate().toISOString(),
-    });
-  });
-  return { path: "meta.json", data: JSON.stringify(meta) };
+	locals.tags.sort("name").each(function (tag) {
+		if (!meta.tags.includes(tag.name)) meta.tags.push(tag.name);
+	});
+	locals.categories.sort("name").each(function (category) {
+		if (!meta.categories.includes(category.name))
+			meta.categories.push(category.name);
+	});
+	locals.posts.sort("name").each(function (post) {
+		meta.posts.push({
+			title: post.title,
+			url: encodeURI(post.permalink),
+			// get modified date first
+			date:
+				post.updated.toDate().toISOString() || post.date.toDate().toISOString(),
+		});
+	});
+	return { path: "meta.json", data: JSON.stringify(meta) };
 });
