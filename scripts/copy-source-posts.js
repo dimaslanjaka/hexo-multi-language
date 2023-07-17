0c242327a720436678cd2e693c627935517e91a5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-17 14:24:02 +0700

    fix: for symlinked folder

--- scripts/copy-source-posts.js
+++ scripts/copy-source-posts.js
@@ -5,9 +5,9 @@ const { path, fs } = require("sbg-utility");
 hexo.extend.filter.register("after_init", function () {
 	const src = path.join(hexo.base_dir, hexo.config.post_dir);
 	const dest = path.join(hexo.base_dir, "source/_posts");
-	fs.copySync(src, dest, { overwrite: true });
-  // logging
+	fs.copySync(src, dest, { overwrite: true, dereference: true });
+	// logging
 	const logsrc = src.replace(path.toUnix(hexo.base_dir), "");
-  const logdest = dest.replace(path.toUnix(hexo.base_dir), "");
+	const logdest = dest.replace(path.toUnix(hexo.base_dir), "");
 	hexo.log.i("init", "copy posts", logsrc, "->", logdest);
 });
